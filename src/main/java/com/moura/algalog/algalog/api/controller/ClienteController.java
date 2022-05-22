35b237dce99bd57d0ad518e35d79c26d1cfe5e32
Mateus Moura <mateus51moura@gmail.com>
2022-05-22 14:07:55 -0300

    Validacao com Bean Validation

--- src/main/java/com/moura/algalog/algalog/api/controller/ClienteController.java
+++ src/main/java/com/moura/algalog/algalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.moura.algalog.algalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,13 +41,13 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
-		@RequestBody Cliente cliente) {
+		@Valid @RequestBody Cliente cliente) {
 		if (!clienteRepository.existsById(clienteId)) {
 			return ResponseEntity.notFound().build();
 		}
