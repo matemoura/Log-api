73852d37939839acf38419caa6dedc7632c93f71
Mateus Moura <mateus51moura@gmail.com>
2022-05-22 01:41:20 -0300

    JPA e implementação do CRUD de cliente

---
+++ src/main/java/com/moura/algalog/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.moura.algalog.algalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.moura.algalog.algalog.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	
+}
--- src/main/java/com/moura/algalog/algalog/model/Cliente.java
+++ src/main/java/com/moura/algalog/algalog/model/Cliente.java
@@ -1,34 +1,72 @@
 package com.moura.algalog.algalog.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	
+	private String nome;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+	
+	public String getNome() {
+		return nome;
 	}
-	public void setName(String name) {
-		this.name = name;
+	
+	public void setName(String nome) {
+		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getTelefone() {
 		return telefone;
 	}
+	
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 }
