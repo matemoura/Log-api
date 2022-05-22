cf110b3a40196882ebdb8f3669a0c0e258cc5535
Mateus Moura <mateus51moura@gmail.com>
2022-05-21 23:01:55 -0300

    Implementacao do Collection Resource

--- src/main/java/com/moura/algalog/algalog/api/controller/ClienteController.java
+++ src/main/java/com/moura/algalog/algalog/api/controller/ClienteController.java
@@ -1,5 +1,30 @@
 package com.moura.algalog.algalog.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moura.algalog.algalog.model.Cliente;
+
+@RestController
 public class ClienteController {
 
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setName("Jõao");
+		cliente1.setTelefone("34 99999-1111");
+		cliente1.setEmail("joao@gmail.com");
+		
+		Cliente cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setName("Maria");
+		cliente2.setTelefone("11 97777-2222");
+		cliente2.setEmail("maria@gmail.com");
+		
+		return Arrays.asList(cliente1, cliente2);
+	}
 }
---
+++ src/main/java/com/moura/algalog/algalog/model/Cliente.java
@@ -0,0 +1,34 @@
+package com.moura.algalog.algalog.model;
+
+public class Cliente {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String telefone;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
